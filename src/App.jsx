bbbc64bc0aa19f017cd6bc1f2b7a3007df7edfea
Mehedi Hasan <mehedih.dev@gmail.com>
2023-06-08 18:26:02 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Axios from 'axios'
+function App() {
+  const [excuse, setExcuse] = useState('')
+  const fetchExcuser=(excuseName)=>{
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuseName}/`)
+    .then((res)=>{
+      setExcuse(res.data[0].excuse)
+    })
+  }
+  return (
+    <div>
+      <h1>Choos the context of excuse in which you want to use</h1>
+      <button onClick={()=>{fetchExcuser('family')}}>Family</button>
+      
+      <button onClick={()=>{fetchExcuser('office')}}>office</button>
+      
+      <button onClick={()=>{fetchExcuser('children')}}>children</button>
+      
+      <button onClick={()=>{fetchExcuser('college')}}>college</button>
+      
+      <button onClick={()=>{fetchExcuser('party')}}>party</button>
+      
+      <button onClick={()=>{fetchExcuser('funny')}}>funny</button>
+      
+      <button onClick={()=>{fetchExcuser('unbelievable')}}>unbelievable</button>
+      
+      <button onClick={()=>{fetchExcuser('developers')}}>developers</button>
+      
+      
+      <button onClick={()=>{fetchExcuser('gaming')}}>gaming</button>
+      
+    
+      <h2>Here goes your excuse: {excuse}</h2>
+    </div>
+  )
+}
+
+export default App
